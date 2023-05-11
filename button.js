a92a1e8454bc5ec0ed1c0868b221772d7147123b
KairiyamaCristian <cristiankairiyama@gmail.com>
2023-05-11 18:06:19 -0300

    poniendo menu

--- button.js
+++
@@ -1,6 +0,0 @@
-const changeBtn = document.querySelector("button");
-
-changeBtn.addEventListener("click", function() {
-  document.body.classList.toggle("dark-mode");
-  changeBtn.classList.toggle("dark-mode");
-});
