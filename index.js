3a4ddf5a3417c05eb1bb9b532a47938c42892032
KairiyamaCristian <cristiankairiyama@gmail.com>
2023-07-06 21:03:50 -0300

    reinicio de parametros

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ changeBtn.addEventListener("click", function() {
 
 function showSection(sectionNumber) {
   var sections = document.getElementsByClassName('section');
-  
+  var navigation = document.getElementById('navigation');
+
   for (var i = 0; i < sections.length; i++) {
     sections[i].classList.remove('active');
   }
-  if (sectionNumber == 0) {
-    document.getElementById('section' + sectionNumber).classList.add('active');
-  }
   if (sectionNumber > 0) {
     document.getElementById('section' + sectionNumber).classList.add('active');
+    navigation.classList.add('hidden');
+  }else {
+    navigation.classList.remove('hidden');
   }
+}
+
+function resetPage() {
+  location.reload();
 }
